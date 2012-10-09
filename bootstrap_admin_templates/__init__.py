2faa4a3b0cb28296b3137ce444724116514955ce
Marcus R. Brown <mrbrown@precision-mojo.com>
2012-10-09 16:13:46 -0700

    Add the boostrap_admin_templates package.

---
+++ bootstrap_admin_templates/__init__.py
@@ -0,0 +1,7 @@
+"""
+django-bootstrap-admin-templates provides Twitter Bootstrap compatible
+templates for use with the django.contrib.admin app.
+"""
+
+version = (0, 1, 0)
+__version__ = '.'.join(map(str, version))
